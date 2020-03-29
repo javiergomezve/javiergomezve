df5c8175e96f208b6426d42c7a58cf3db06c40ce
Javier Gómez <javiergomezve@gmail.com>
2020-03-29 16:05:56 -0500

    [add] ContactForm with styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 import imgPromo from '../images/promo.svg';
+import ContactForm from './ContactForm';
 
 export default () => (
     <header className="bg-yellow-300">
-        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
-            <div className="flex-1">
-                <h1 className="text-6xl font-bold text-yellow-900">¡Hola! Soy Javier</h1>
-                <p className="text-xl font-light">Creo aplicaciones web.</p>
+        <div className="container mx-auto p-12 max-w-4xl">
+            <div className="flex justify-center items-center">
+                <div className="flex-1">
+                    <h1 className="text-6xl font-bold text-yellow-900">¡Hola! Soy Javier</h1>
+                    <p className="text-xl font-light">Creo aplicaciones web.</p>
+                </div>
+
+                <img src={imgPromo} alt="Un programador caminando" style={{ height: '300px'}}/>
             </div>
-            <img src={imgPromo} alt="Un programador caminando" style={{ height: '300px'}}/>
+
+            <div>
+                <ContactForm/>
+            </div>
         </div>
     </header>
 );
