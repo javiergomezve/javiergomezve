332f60f460833563acdaafc41cbb8d14973ecd02
Javier Gómez <javiergomezve@gmail.com>
2020-03-29 21:44:34 -0500

    [add] responsive on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import ContactForm from './ContactForm';
 
 export default () => (
     <header className="bg-yellow-300">
-        <div className="container mx-auto p-12 max-w-4xl">
+        <div className="container mx-auto p-5 md:p-12 max-w-4xl">
             <div className="flex flex-col md:flex-row justify-center items-center">
                 <div className="flex-1">
                     <h1 className="text-6xl font-bold text-yellow-900">¡Hola! <br/> Soy Javier</h1>
